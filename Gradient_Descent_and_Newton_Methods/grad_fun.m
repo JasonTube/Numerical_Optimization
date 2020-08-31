4a64efca36ae3486b3bd95d156d02ef859db4e51
Jinyang Jiang <49957876+JasonTube@users.noreply.github.com>
2020-08-31 10:25:16 +0800

    Update grad_fun.m

--- Gradient_Descent_and_Newton_Methods/grad_fun.m
+++ Gradient_Descent_and_Newton_Methods/grad_fun.m
@@ -1,3 +1,3 @@
-function df = df_func(x)
-df = [ 400*x(1)*(x(1)^2-x(2)) + 2*(x(1)-1)
-    -200*(x(1)^2-x(2)) ];
+function grad = grad_fun(x)
+grad = [ 400*x(1)*(x(1)^2-x(2)) + 2*(x(1)-1)
+        -200*(x(1)^2-x(2)) ];
