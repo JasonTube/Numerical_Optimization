494dfdb1ac45ec661514923a4705e4258a81323e
JasonTube <843070296@qq.com>
2020-08-31 10:34:00 +0800

    delete old method

--- Trust_Region_Method/JacobiH.m
+++
@@ -1,8 +0,0 @@
-function A = JacobiH(mu,lamda,d,Bk,delta)
-n = length(d);
-A = zeros(n+2,n+2);
-pmu = -4*mu/sqrt((lamda+norm(d)^2-delta^2)^2+4*mu^2);
-thetak = (lamda+norm(d)^2-delta^2)/sqrt((lamda+norm(d)^2-delta^2)^2+4*mu^2);
-A = [   1,          0,          zeros(1,n)
-        pmu,        1-thetak,   -2*(1+thetak)*d'
-        zeros(n,1), d,          Bk+lamda*eye(n)];
--- Trust_Region_Method/dah.m
+++
@@ -1,9 +0,0 @@
-function dh = dah(mu,lamda,d,gk,Bk,delta)
-n =length(d);
-dh(1) = mu;
-dh(2) = phi(mu,lamda,delta^2-norm(d)^2);
-mh = (Bk+lamda*eye(n))*d+gk;
-for i = 1:n
-    dh(2+i) = mh(i);
-end
-dh = dh(:); % to gurantee that the output is a column vector
